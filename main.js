3c76b3ed25fd6ad44d787f022d6d90c7b67b87d8
Jared Johnson <60166120+Ghostman80@users.noreply.github.com>
2021-01-07 01:30:32 -0700

    Add files via upload
    This is a website on free code camp with some small changes to the code.

---
+++ main.js
@@ -0,0 +1,5 @@
+// Modifed Navbar color to a gray color.
+// Changing the footer and nav color to orange when you hover over a link.
+// This web is from freecodecamp.com and is not my website. I modified some of the code for learning purposes only.
+
+const projectName = 'product-landing-page';
